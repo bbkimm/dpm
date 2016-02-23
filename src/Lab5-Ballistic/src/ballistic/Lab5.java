37a46b5e195056e77de74b216c29d882977a8a44
bbkimm <brianjkl240@gmail.com>
2016-02-23 11:07:33 -0500

    Two motor setup, tweaked values for working, accurate launcher

--- src/Lab5-Ballistic/src/ballistic/Lab5.java
+++ src/Lab5-Ballistic/src/ballistic/Lab5.java
@@ -1,37 +1,58 @@
+/*
+ * Group 51
+ * Brian Kim-Lim (260636766)
+ * Jason Dias (260617554)
+ * 
+ * Lab 5 - Ballistics
+ */
+
 package ballistic;
 
 import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.Port;
-
+import lejos.utility.Delay;
 import lejos.hardware.Button;
 
-//
-// Lab 1:  EV3 Wall Following robot
-//
-// This is the main class for the wall follower.
 
 public class Lab5 {
 
 	private static final Port usPort = LocalEV3.get().getPort("A");
-	private static final EV3LargeRegulatedMotor launchMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("A"));
+	private static final EV3LargeRegulatedMotor launchMotorOne = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("B"));
+	private static final EV3LargeRegulatedMotor launchMotorTwo = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("C"));
 
 	public static void main(String[] args) {
 		//setup
 		
-
+		
 		while (true) {
 			
 			Button.waitForAnyPress(); // wait for button to release
-			launchMotor.setSpeed(2000);
-			launchMotor.setAcceleration(20000);
 			
-			launchMotor.rotate(-65);
-			launchMotor.setSpeed(50);
-			launchMotor.rotate(65);
+			//High speed and acceleration is needed to have enough power to launch the ball at ~120cm away from
+			//the objective tile
+			launchMotorOne.setSpeed(5500);
+			launchMotorOne.setAcceleration(100000);
+			launchMotorTwo.setSpeed(5500);
+			launchMotorTwo.setAcceleration(100000);
+			
+			//From our starting angle position, rotate 80 degrees to reach roughly the optimal launch angle
+			launchMotorOne.rotate(-80,true);
+			launchMotorTwo.rotate(-80,false);
+			
+			//Stop motors to help avoid floating motors and an oscillating catapult arm
+			launchMotorOne.stop();
+			launchMotorTwo.stop();
+			
+			//Lowers the arm for re-use
+			launchMotorOne.setSpeed(50);
+			launchMotorTwo.setSpeed(50);
+			launchMotorOne.rotate(80,true);
+			launchMotorTwo.rotate(80,false);
 			
 			
 		}
+		
 
 	}
 }
